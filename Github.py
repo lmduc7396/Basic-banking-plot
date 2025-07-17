79d57a6ca77214c9fea8f7174670ed5f0bee915f
lmduc7396 <lmduc7396@gmail.com>
2025-07-17 17:52:51 +0700

    Update Github.py

--- Github.py
+++ Github.py
@@ -91,7 +91,7 @@ for idx, z_name in enumerate(Z):
 
 fig.update_layout(
     width=1400,
-    height=2000,
+    height=1400,
     title_text=f"Banking Metrics: {', '.join(Z)}",
     legend_title="Ticker/Type"
 )
