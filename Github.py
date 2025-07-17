732ce8009fcf0da31addb28963362bc23d222ff6
lmduc7396 <lmduc7396@gmail.com>
2025-07-17 17:42:20 +0700

    Update Github.py

--- Github.py
+++ Github.py
@@ -32,7 +32,7 @@ Y = st.sidebar.number_input("Number of latest periods to plot (Y):", min_value=1
 Z = st.sidebar.multiselect(
     "Select Value Column(s) (Z):", 
     keyitem['Name'].tolist(),
-    default = ['NIM','Loan yield','NPL','GROUP 2']
+    default = ['NIM','Loan yield','NPL','GROUP 2','NPL Formation (%)', 'G2 Formation (%)']
 )
 
 #Setup subplot
