0b1b962cd102e194b80fb48fa69697a5ec7b72e4
lmduc7396 <lmduc7396@gmail.com>
2025-07-18 13:54:50 +0700

    Update Github.py

--- Github.py
+++ Github.py
@@ -297,7 +297,7 @@ def conditional_format(df):
             return [str(v) if v is not None else "" for v in row]
         median_val = np.median(np.abs(numeric_vals))
         if median_val > 100:
-            return ["{:,.2s}".format(float(v)) if pd.notnull(v) and v != '' else "" for v in row]
+            return ["{:,}".format(float(v)) if pd.notnull(v) and v != '' else "" for v in row]
         else:
             return ["{:.2f}%".format(float(v)) if pd.notnull(v) and v != '' else "" for v in row]
             
